7581c208ecd01dac120cd70d00fd0959f115796e
Alan <ajohnson@themorangroup.net>
2021-07-26 15:11:48 -0400

    Style brand top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,15 +4,19 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 export const TopBar: FC<{}> = () => {
   return (
-    <div className="font-mono h-20 bg-green">
-      <StaticImage
-        className="rounded-full"
-        src="../images/logo.jpg"
-        alt="Logo image - Brooklyn Bridge"
-        width={50}
-        height={50}
-      />
-      Brooklyn Development Co.
+    <div className="font-mono h-24 bg-green flex items-center border-b border-gray-400">
+      <div className="rounded-full w-16 h-16 ml-5 mr-5 overflow-hidden bg-white border border-black">
+        <StaticImage
+          className="relative top-2"
+          src="../images/logo.jpg"
+          alt="Logo - Brooklyn Bridge"
+          width={75}
+          height={50}
+        />
+      </div>
+      <h1 className="font-sans text-3xl text-white font-semibold tracking-wider">
+        BROOKLYN DEVELOPMENT CO.
+      </h1>
     </div>
   );
 };
