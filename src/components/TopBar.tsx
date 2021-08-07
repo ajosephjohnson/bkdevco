8282bbd6eb3de1dd33a6140c582522d12cdc217e
Alan <ajosephjohnson@gmail.com>
2021-08-06 21:49:47 -0400

    Refine hover styles on nav section links

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -17,7 +17,7 @@ export const TopBar: FC<{}> = () => {
               height={50}
             />
           </div>
-          <h1 className="font-sans text-2xl ml-5 text-white font-bold border-b-2 border-transparent group-hover:border-white">
+          <h1 className="font-sans text-2xl ml-5 text-white font-bold border-b-2 border-transparent group-hover:border-theme-light-green transition-colors transition-border-b group-hover:text-theme-light-green">
             BROOKLYN
             <br />
             DEVELOPMENT CO.
@@ -27,21 +27,21 @@ export const TopBar: FC<{}> = () => {
           <li className="mx-4">
             <a
               href="#services"
-              className="px-2 py-1 border-b-2 border-transparent hover:border-white">
+              className="px-2 py-1 border-b-2 border-transparent hover:border-theme-light-green transition-colors transition-border-b hover:text-theme-light-green">
               SERVICES
             </a>
           </li>
           <li className="mx-4">
             <a
               href="#testimonials"
-              className="px-2 py-1 border-b-2 border-transparent hover:border-white">
+              className="px-2 py-1 border-b-2 border-transparent hover:border-theme-light-green transition-colors transition-border-b hover:text-theme-light-green">
               TESTIMONIALS
             </a>
           </li>
           <li className="mx-4">
             <a
               href="#past-projects"
-              className="px-2 py-1 border-b-2 border-transparent hover:border-white">
+              className="px-2 py-1 border-b-2 border-transparent hover:border-theme-light-green transition-colors transition-border-b hover:text-theme-light-green">
               PAST PROJECTS
             </a>
           </li>
@@ -52,7 +52,7 @@ export const TopBar: FC<{}> = () => {
           rel="noopener noreferrer">
           <button
             type="button"
-            className="mx-4 px-5 py-2 bg-white shadow-2xl font-bold text-lg text-gray-800 rounded-full transition-colors border border-gray-800 hover:bg-theme-light-green hover:text-gray-900 hover:border-gray-900">
+            className="mx-4 px-5 py-2 bg-white shadow-2xl font-bold text-lg text-gray-800 rounded-full transition-colors border border-gray-800 hover:bg-theme-light-green hover:text-gray-600 hover:border-gray-600">
             SCHEDULE FREE CONSULTATION
           </button>
         </a>
