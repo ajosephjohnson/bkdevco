f28eb8b2e78a652ec526ff77381f92a0ed876c2b
Alan <ajosephjohnson@gmail.com>
2021-08-08 13:24:45 -0400

    Change bash variable names

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-XSH_USER_ID=$(id -u ${USER})
-XSH_USER_GROUP=$(id -g ${USER})
-docker-compose exec -u $XSH_USER_ID:$XSH_USER_GROUP app /bin/bash
+USER_ID=$(id -u ${USER})
+USER_GROUP=$(id -g ${USER})
+docker-compose exec -u $USER_ID:$USER_GROUP app /bin/bash
--- scripts/type-check.sh
+++ scripts/type-check.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-XSH_USER_ID=$(id -u ${USER})
-XSH_USER_GROUP=$(id -g ${USER})
-docker-compose run --rm -u $XSH_USER_ID:$XSH_USER_GROUP app yarn run type-check
+docker-compose run --rm app yarn run type-check
