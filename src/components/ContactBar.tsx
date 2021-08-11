5a2847b126acbeaa794bf385a165a1acd495ff28
Alan <ajosephjohnson@gmail.com>
2021-08-11 00:15:22 -0400

    Make hover effects more prominent

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -15,7 +15,7 @@ export const ContactBar: FC<{}> = () => {
     <div className="bg-theme-primary-green font-sans text-2xl font-bold">
       <address className="h-14 w-1/2 mx-auto flex justify-around items-center not-italic">
         <a
-          className="hover:opacity-90 transition-opacity text-white py-1 px-1"
+          className="hover:opacity-80 transition-opacity text-white py-1 px-1"
           href="mailto:alan@bkdev.co"
           rel="noopener noreferrer"
           target="_blank">
@@ -23,7 +23,7 @@ export const ContactBar: FC<{}> = () => {
           alan@bkdev.co
         </a>
         <a
-          className="hover:opacity-90 transition-opacity text-white py-1 px-1"
+          className="hover:opacity-80 transition-opacity text-white py-1 px-1"
           href="tel:+1-347-508-5787"
           rel="noopener noreferrer"
           target="_blank">
@@ -36,7 +36,7 @@ export const ContactBar: FC<{}> = () => {
           ).map(socialMediaType => (
             <a
               key={socialMediaType}
-              className="hover:opacity-90 transition-opacity py-1 px-1"
+              className="hover:opacity-80 transition-opacity py-1 px-1"
               href={SOCIAL_MEDIA_LINKS[socialMediaType]}
               rel="noopener noreferrer"
               target="_blank">
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -9,7 +9,7 @@ export const TopBar: FC<{}> = () => {
   return (
     <div className="fixed bg-transparent z-20 w-full font-sans">
       <div className="container px-5 h-24 mx-auto flex items-center">
-        <Link to="/" className="px-1 py-1 transition-opacity hover:opacity-90">
+        <Link to="/" className="px-1 py-1 transition-opacity hover:opacity-80">
           <StaticImage
             src="https://s3.amazonaws.com/bkdev.co/logo-branding-white.png"
             alt="Main logo - Brooklyn Development Co."
@@ -22,7 +22,7 @@ export const TopBar: FC<{}> = () => {
             <li className="mx-4" key={id}>
               <a
                 href={`#${id}`}
-                className="px-2 py-1 border-b-2 border-transparent hover:border-white transition hover:opacity-90">
+                className="px-2 py-1 border-b-2 border-transparent hover:border-white transition hover:opacity-80">
                 {id.split('-').join(' ').toUpperCase()}
               </a>
             </li>
@@ -34,7 +34,7 @@ export const TopBar: FC<{}> = () => {
           rel="noopener noreferrer">
           <button
             type="button"
-            className="ml-4 px-5 py-2 bg-white shadow-2xl font-bold text-lg text-gray-800 rounded-full transition-opacity border border-gray-800 hover:opacity-90">
+            className="ml-4 px-5 py-2 bg-white shadow-2xl font-bold text-lg text-gray-800 rounded-full transition-opacity border border-gray-800 hover:opacity-80">
             SCHEDULE FREE CONSULTATION
           </button>
         </a>
